097e6fa3b31a54f0b63877e2fbdf138313862c76
hasib-stack <hellosas307@gmail.com>
2026-05-18 18:48:21 +0600

    supabase.js

---
+++ supabase.js
@@ -0,0 +1,11 @@
+const SUPABASE_URL =
+"https://lwuwymojztivmiigxaka.supabase.co/rest/v1/";
+
+const SUPABASE_KEY =
+"sb_secret_upuKSzTMnK2AkXE2EkWILg_BNO-JPod";
+
+const supabase =
+window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
